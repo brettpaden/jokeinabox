b31bfedd955af5fbcf04c855b57373b4de1ce87b
Brett Paden <paden@pkt3.com>
2012-02-28 10:15:05 -0500

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jokeinabox::Application.config.secret_token = '194f1672a8b362b1770525ce7c9be572595eb901b92a8f63f25fa477516a5eba016190929f3f4afefcc1091f9dbb06117c167337f9d3a8d3655df36d57c18217'
