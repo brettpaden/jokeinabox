3447ccacf9e2a60e4017db75e86249cea5713c33
Peter Pezaris <pez@pkt3.com>
2012-03-05 19:07:56 -0500

    newtest

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120228232233) do
 
-  create_table "posts", :force => true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_hash"
